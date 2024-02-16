0bf638faefc73d4c8ef25881f32d58a7c0aa44b7
iris <a1158288818@icloud.com>
2024-02-16 11:45:08 -0500

    update sdk

--- assets/js/sdk/apigClient.js
+++ assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://jkxcdf0ate.execute-api.us-east-1.amazonaws.com/stage1';
+    var invokeUrl = 'https://bkebhlytc8.execute-api.us-east-1.amazonaws.com/stage1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
